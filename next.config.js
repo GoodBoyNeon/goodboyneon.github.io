a42a336dfaa7bb50ac94658bb6880ad1452cafbf
GoodBoyNeon <goodboyneon03@gmail.com>
2023-09-12 17:16:37 +0545

    fix(next-config): update BasePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "goodboyneon.github.io",
+  basePath: "/goodboyneon.github.io",
 };
 
 module.exports = nextConfig;
