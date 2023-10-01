91b05d697e988a8da74f89c83726962733ae94f6
GoodBoyNeon <goodboyneon03@gmail.com>
2023-10-01 15:55:06 +0545

    fix: import `useReducedMotion`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 'use client';
 
-import {
-  Hero,
-  About,
-  Stars,
-  Contact,
-  Footer,
-  RedirectModal,
-  Skills,
-  Projects,
-} from '@components';
+import { Hero, About, Stars, Contact, Footer, Skills, Projects } from '@components';
+import { useReducedMotion } from '../hooks';
 
 export default function Home() {
   const prefersReducedMotion = useReducedMotion();
   return (
     <div>
       {!prefersReducedMotion && <Stars />}
-      {/* <RedirectModal /> */}
       <Hero />
       <About />
       <Skills />
