40c70d798c18a6cdd31f23ca119d82bc8aebcb93
GoodBoyNeon <goodboyneon03@gmail.com>
2023-10-03 17:19:53 +0545

    fix: tweak footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@
 
 import Link from 'next/link';
 import { Octokit } from 'octokit';
-import React, { useEffect, useState } from 'react';
-import { BiGitRepoForked, BiLinkExternal, BiStar } from 'react-icons/bi';
-import { JetBrainsMono } from '../utils';
+import { useEffect, useState } from 'react';
 import { AiFillGithub, AiFillHeart } from 'react-icons/ai';
+import { BiGitRepoForked, BiStar } from 'react-icons/bi';
+import { JetBrainsMono } from '../utils';
 
 type GitStats = {
   stars?: number;
@@ -33,29 +33,29 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex flex-col items-center justify-center bg-background">
-
-      <hr className=" h-[1px] w-screen border-0 bg-bg-light text-bg-light" />
-      <Link
-        className={`${JetBrainsMono.className} mt-4 flex w-full max-w-fit flex-col items-center justify-center text-subtext transition-all duration-300 hover:text-primary`}
-        href={'https://github.com/goodboyneon/goodboyneon.github.io'}
-      >
-        <div className="flex flex-row align-middle">
-          <AiFillGithub size={20} />{' '}
-          <span className="align-bottom">Made with</span>
-          <AiFillHeart size={20} />{' '}
-          <span className="align-bottom">by Neon!</span>
-        </div>
-        <div className="flex w-20 justify-between font-normal">
-          <span className="flex flex-row">
-            <BiStar size={20}/>{' '}
-            {stars && stars}
-          </span>
-          <span className="flex flex-row">
-            <BiGitRepoForked size={20} />{' '}
-            {forks && forks}
-          </span>
-        </div>
-      </Link>
+        <hr className=" h-[1px] w-screen border-0 bg-bg-light text-bg-light" />
+        <Link
+          className={`${JetBrainsMono.className} mt-4 flex flex-col items-center justify-center text-subtext transition-all duration-300 hover:text-primary`}
+          href={'https://github.com/goodboyneon/goodboyneon.github.io'}
+        >
+          <div className="flex flex-row space-x-1 align-middle">
+            <span className="flex gap-2">
+              <AiFillGithub size={20} className="" />
+            </span>
+            <span className="align-bottom">Made with</span>
+            <AiFillHeart size={20} /> <span className="align-bottom">by Neon!</span>
+          </div>
+          <div className="flex w-20 justify-between space-x-1 font-normal">
+            <span className="flex flex-row">
+              <BiStar size={20} />
+              <span>{stars && stars}</span>
+            </span>
+            <span className="flex flex-row">
+              <BiGitRepoForked size={20} />
+              <span>{forks && forks}</span>
+            </span>
+          </div>
+        </Link>
       </div>
       <p className="pb-1 pr-2 text-right text-subtext">&copy; All rights reserved</p>
     </footer>
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -7,20 +7,24 @@ import {
 
 export const FiraSans = Fira_Sans({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  preload: false,
 });
 
 export const DelaGothicOne = Dela_Gothic_One({
   subsets: ['latin'],
   weight: ['400'],
+  preload: false,
 });
 
 export const JetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800'],
+  weight: ['400'],
+  preload: false,
 });
 
 export const Rubik = Rubik_Font({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800', '900'],
+  preload: false,
 });
